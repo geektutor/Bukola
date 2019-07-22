7ce885d03ac62ffa4a17c67365afa9253a680d05
Geektutor <sodiq.akinjobi@gmail.com>
2019-07-22 18:03:12 +0100

    Updated

--- templates/temp.php
+++ templates/temp.php
@@ -4,29 +4,27 @@
 */
 do_action( 'genesis_doctype' );
 do_action( 'genesis_title' );
-// do_action( 'genesis_meta' ); //* Uncomment this if you want child theme's style.css to load
+do_action( 'genesis_meta' ); //* comment this if you dont want child theme's style.css to load
 wp_head(); //* we need this for plugins
 ?>
 <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://unilagconsult.com.ng/cybercamp19/wp/css/style.css">
+    <link rel="stylesheet" href="">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Page Template</title>
+    <title>Template Page</title>
 </head>
 
 <body>
 
-
-<!-- Body of the code here
-
--->
+<!-- Body of the code here-->
 
 
+</body>
 
   <?php
 	wp_footer(); //* we need this for plugins
 	?>
 </body>
-<script src="https://unilagconsult.com.ng/cybercamp19/wp/js/index.js"></script>
+<script src="a.js"></script>
 </html>
